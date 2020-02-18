791ece2add4a2b5cd73d21fc02519f9d76e3ba4c
Miguel Angel Rodriguez Perez <miguelachoo@gmail.com>
2020-02-18 14:21:21 -0300

    removing indentation and using 'useDispatch' to use redux actions

--- TodoItem.js
+++ TodoItem.js
@@ -2,37 +2,36 @@ import React from 'react';
 import { StyleSheet, View, Text, Button } from 'react-native';
 import {ChangeStatus, DeleteTodo} from './actions';
 import styles from './Styling';
-import {connect} from 'react-redux';
+import {useDispatch} from 'react-redux';
 
 const Todo = function(props){
-    let todoItem;
-    if(props.done){
-      todoItem = StyleSheet.flatten([styles.todoItem, styles.done]);
-    }else{
-      todoItem = StyleSheet.flatten([styles.todoItem, styles.notdone]);
-    }
-    return (
-      <View style={todoItem}>
-        <Text
-          style={styles.todoDescription}
-          data-id={props.id}
-          data-name={props.name}>
-            {props.name}
-        </Text>
-        <View style={styles.todoActions}>
-          <View style={{marginRight:5}}>
-            <Button
-              title="mark as done"
-              onPress={() => props.ChangeStatus(props.id)}/>
-          </View>
+  const dispatch = useDispatch();
+  let todoItem;
+  if(props.done){
+    todoItem = StyleSheet.flatten([styles.todoItem, styles.done]);
+  }else{
+    todoItem = StyleSheet.flatten([styles.todoItem, styles.notdone]);
+  }
+  return (
+    <View style={todoItem}>
+      <Text
+        style={styles.todoDescription}
+        data-id={props.id}
+        data-name={props.name}>
+          {props.name}
+      </Text>
+      <View style={styles.todoActions}>
+        <View style={{marginRight:5}}>
           <Button
-            title="remove"
-            onPress={() => props.DeleteTodo(props.id)}/>
+            title="mark as done"
+            onPress={() => dispatch(ChangeStatus(props.id))}/>
         </View>
+        <Button
+          title="remove"
+          onPress={() => dispatch(DeleteTodo(props.id))}/>
       </View>
-    );
+    </View>
+  );
 }
 
-export default connect(null, {
-    ChangeStatus, DeleteTodo
-})(Todo);
+export default Todo;
