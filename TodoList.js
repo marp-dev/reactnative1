982dddf65b9988d82428c6597c5b47fcd75c2ad7
Miguel Angel Rodriguez Perez <miguelachoo@gmail.com>
2020-02-18 14:20:35 -0300

    fixing data sent to TodoItem. replacing 'connect' with 'useSelector' to read the redux state.

--- TodoList.js
+++ TodoList.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { View, FlatList } from 'react-native';
-import {connect} from 'react-redux';
+import {useSelector} from 'react-redux';
 import _ from 'lodash';
 import styles from './Styling';
 import Todo from './TodoItem';
 
 const TodoList = function(props){
+  const todos = useSelector((state) => state.list);
   return (
     <View style={styles.todolist}>
       <FlatList
-        data={props.list}
-        renderItem={(item) => <Todo {...item} />}
+        data={todos}
+        renderItem={({item}) => <Todo {...item} />}
         keyExtractor={(item) => item.id}/>
     </View>
   );
 
 };
 
-const mapStateToProps = (state, props) => {
-  return {
-    list: state.list
-  };
-};
-export default connect(mapStateToProps)(TodoList);
+export default TodoList;
