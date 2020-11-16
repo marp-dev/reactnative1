7c73e1ba9a092dcff4c5c13893aa44ad009bbb79
Miguel Angel Rodriguez Perez <miguelachoo@gmail.com>
2020-11-16 19:38:33 -0300

    fixing sh commands

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 tmux new -d -s react expo start
 tmux new -d -s server json-server --host 0.0.0.0 --watch ./db.json
--- stop.sh
+++ stop.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 tmux kill-session -t react
 tmux kill-session -t server
