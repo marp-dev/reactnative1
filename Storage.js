861a706a0bc414a0f16c0cc706a40beab42c0f19
Miguel Angel Rodriguez Perez <info@marp.rocks>
2023-02-14 14:51:04 -0300

    removing ';' in Storage.js

--- Storage.js
+++ Storage.js
@@ -1,19 +1,19 @@
-import {useState, useEffect} from 'react';
-import {Platform} from 'react-native';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import reducers from './reducers';
-import {LoadFromServer, LoadDataSource} from './actions';
-import {useDispatch, useSelector} from 'react-redux';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import {useState, useEffect} from 'react'
+import {Platform} from 'react-native'
+import {createStore, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
+import {LoadFromServer, LoadDataSource} from './actions'
+import {useDispatch, useSelector} from 'react-redux'
+import * as Font from 'expo-font'
+import { Ionicons } from '@expo/vector-icons'
 
 export const store = createStore(reducers, applyMiddleware(thunk))
 
 export const useLoadData = () => {
-    const [serverLoaded, setServerLoaded] = useState(false);
-    const [fontLoaded, setFontLoaded] = useState(true);
-    const dispatch = useDispatch();
+    const [serverLoaded, setServerLoaded] = useState(false)
+    const [fontLoaded, setFontLoaded] = useState(true)
+    const dispatch = useDispatch()
     const data_source = useSelector((state) => state.data_source)
 
     useEffect(async () => {
@@ -27,10 +27,10 @@ export const useLoadData = () => {
 
     useEffect(() => {
         if(data_source == 'server')
-            dispatch(LoadFromServer(() => setServerLoaded(true)));
-    }, [data_source]);
+            dispatch(LoadFromServer(() => setServerLoaded(true)))
+    }, [data_source])
 
-    return {serverLoaded, fontLoaded};
+    return {serverLoaded, fontLoaded}
 }
 
 export default store
