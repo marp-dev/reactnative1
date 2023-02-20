1b4764776d6a8bd8edbfbf82a051321b8f5cf200
Miguel Angel Rodriguez Perez <info@marp.rocks>
2023-02-20 15:45:26 -0300

    adjusting url test

--- utils.js
+++ utils.js
@@ -43,5 +43,5 @@ export const timestamp = function(){
 }
 
 export const test_url = function(url){
-    return /^(https|http):\/\/(localhost|[a-z\-\_0-9]+(\.[a-z\-\_0-9]+)+)(:[0-9][0-9][0-9]?[0-9]?)?\/?$/gm.test(url)
+    return /^(https|http):\/\/(localhost|[a-z\-\_0-9]+(\.[a-z\-\_0-9]+)+)(:[0-9][0-9][0-9]?[0-9]?[0-9]?)?\/?$/gm.test(url)
 }
